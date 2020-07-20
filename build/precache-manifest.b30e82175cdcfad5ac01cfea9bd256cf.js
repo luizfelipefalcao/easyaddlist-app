10035be7c3b3873264a78187b0309756f71826be
Luiz Felipe de Jesus Falcao <falcao.lfj@gmail.com>
2020-07-20 20:53:01 -0300

    readme fixed

---
+++ build/precache-manifest.b30e82175cdcfad5ac01cfea9bd256cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b390fb76ae7a1fbc43b0e2d875c815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94f3c63dd01329edfb8",
+    "url": "/static/js/2.35b058f7.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.35b058f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c64e77c6b9acba4dd3",
+    "url": "/static/js/main.c981482c.chunk.js"
+  },
+  {
+    "revision": "508c66d0078142ac7a56",
+    "url": "/static/js/runtime-main.5e661c5e.js"
+  }
+]);
